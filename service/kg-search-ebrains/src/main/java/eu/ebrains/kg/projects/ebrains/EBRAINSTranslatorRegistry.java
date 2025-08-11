6cad94519ae4124ad25913ea07308d47e494290b
Oliver Schmid <oliver.schmid@cscs.ch>
2025-08-11 18:08:31 +0200

    prioritize learning resources in translation

--- service/kg-search-ebrains/src/main/java/eu/ebrains/kg/projects/ebrains/EBRAINSTranslatorRegistry.java
+++ service/kg-search-ebrains/src/main/java/eu/ebrains/kg/projects/ebrains/EBRAINSTranslatorRegistry.java
@@ -39,6 +39,7 @@ import java.util.Optional;
 @Profile("ebrains")
 public class EBRAINSTranslatorRegistry implements TranslatorRegistry {
     private final List<TranslatorModel<?,?>> TRANSLATORS = Arrays.asList(
+            new TranslatorModel<>(LearningResource.class, new LearningResourceTranslator(), false, 1000, true),
             new TranslatorModel<>(Project.class, new ProjectTranslator(), false, 1000, true),
             new TranslatorModel<>(Dataset.class, new DatasetTranslator(), false, 1000, true),
             new TranslatorModel<>(DatasetVersion.class, new DatasetVersionTranslator(), false, 1, true),
@@ -51,7 +52,6 @@ public class EBRAINSTranslatorRegistry implements TranslatorRegistry {
             new TranslatorModel<>(SoftwareVersion.class, new SoftwareVersionTranslator(), false, 1000, true),
             new TranslatorModel<>(WebService.class, new WebServiceTranslator(), false, 1000, true),
             new TranslatorModel<>(WebServiceVersion.class, new WebServiceVersionTranslator(), false, 1000, true),
-            new TranslatorModel<>(LearningResource.class, new LearningResourceTranslator(), false, 1000, true),
             new TranslatorModel<>(Contributor.class, new ContributorTranslator(), false, 1000, true),
             new TranslatorModel<>(ControlledTerm.class, new ControlledTermTranslator(), false, 1000, true),
             new TranslatorModel<>(ContentType.class, new ContentTypeTranslator(), false, 1000, false),
