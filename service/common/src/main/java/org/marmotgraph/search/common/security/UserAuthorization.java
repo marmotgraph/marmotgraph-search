16ca34a2d6b94fe930030eeabcea0c89446873e2
Oliver Schmid <oliver.schmid@cscs.ch>
2026-02-10 09:47:46 +0100

    prepare library for publication

---
+++ service/common/src/main/java/org/marmotgraph/search/common/security/UserAuthorization.java
@@ -0,0 +1,22 @@
+package org.marmotgraph.search.common.security;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+
+import javax.swing.*;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+public interface UserAuthorization {
+
+    Set<? extends GrantedAuthority> extractAuthorities(OAuth2User oAuth2User);
+
+    boolean canReadLiveFiles(JwtAuthenticationToken token, UUID repositoryUUID);
+
+    record GroupResponse(String value, String label){}
+
+    List<GroupResponse> getGroups(JwtAuthenticationToken token);
+}
